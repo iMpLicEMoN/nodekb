0221454e13dc1c8aea6aece731346136fb5dc447
Mukhtar Musaev <m89821395289@yandex.ru>
2018-03-16 15:36:56 +0500

    register

--- app.js
+++ app.js
@@ -83,7 +83,9 @@ app.get('/', function(req, res){
 
 // route Files
 let articles = require('./routes/articles');
+let users = require('./routes/users');
 app.use('/articles', articles);
+app.use('/users', users);
 
 // Start Server
 app.listen(3000, function(){
